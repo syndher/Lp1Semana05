bfad0e3aba9b9fb48db9b37ca7ec74a579d0d092
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-19 15:36:54 +0000

    Add tux.jpg

---
+++ LevelOne/LevelTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System ;
+using System.Text;
+using System.Globalization;
+using Spectre.Console;
+
+    namespace LevelTwo
+    {
+        class Program
+        {
+
+        static void Main (string[] args)
+            {   
+                CanvasImage image = new CanvasImage("..\\tux.jpg");
+                image.MaxWidth(20);
+                if (args.Length > 0) {
+                    image = new CanvasImage("..\\" + args[0]);
+                    image.MaxWidth(int.Parse(args[1]));
+                }
+                AnsiConsole.Write(image);
+
+
+            
+                
+            }
+        }
+    }
