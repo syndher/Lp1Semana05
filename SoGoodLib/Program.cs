b8a3a27056ded6d562fb96348262d37bff0c4c37
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-19 15:45:18 +0000

    Add SoGoodLib

---
+++ SoGoodLib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System ;
+using System.Text;
+using System.Globalization;
+using Spectre.Console;
+
+    namespace SoGoodLib
+    {
+        class Class1
+        {
+
+        public static string SoGoodMethod()
+            {   
+                Console.WriteLine("I Came from another project!")  
+            }
+        }
+    }
