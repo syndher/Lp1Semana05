7f2aed95bc81777e2abdefe98b6c0da34664a6ae
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-20 23:26:12 +0000

    Add MainIsHere code

--- LevelOne/MainIsHere.cs
+++ LevelOne/MainIsHere.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿using System;
+
+namespace LevelOne
+{
+    /// <summary>
+    /// Main method is here
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// This is the Main() method
+        /// </summary>
+        /// <param name="args">Console arguments go here</param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+        /// <summary>
+        /// Cool Feature method
+        /// </summary>
+        /// <param name="feature_string">Cool Feature print</param>
+        /// <returns>feature_string</returns>
+        public static string NewFeature(string feature_string)
+        {
+            feature_string = "Cool feature";
+            return feature_string;
+        }
+    }
+}
