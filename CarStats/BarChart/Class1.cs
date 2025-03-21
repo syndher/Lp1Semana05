f404a3d6e9d845e41e8af208e5aa3f12e5807f82
Miguel Filipe <miguelfilipe.dev@gmail.com>
2025-03-21 17:32:55 +0000

    Added randomizers

--- CarStats/BarChart/Class1.cs
+++ CarStats/BarChart/Class1.cs
@@ -2,8 +2,13 @@
 
 namespace BarChart
 {
-    public class Class1
+    public class BarChart()
     {
-
+        string car = new[] {"Ferrari", "Dacia", "Smart", "Lamborghini", "Renault"};
+        int orderid = 0;
+        int orders = new Faker<Order>()
+        .RuleFor(o => o.Item, f => f.PickRandom(car));
+        return f;
+        
     }
 }
--- CarStats/Program.cs
+++ CarStats/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System ;
+using System.Text;
+using System.Globalization;
+using Spectre.Console;
+using BarChart;
+using Bogus;
+
+    namespace CarStats
+    {
+        class Program
+        {
+
+        static void Main (string[] args)
+            {   
+                Console.WriteLine(BarChart.car);
+
+            
+                
+            }
+        }
+    }
